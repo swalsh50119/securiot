ae8b92be14a857015520f4ba3f5cd665aaff0750
Sean Walsh <swalsh50119@gmail.com>
2014-04-24 23:43:54 -0400

    here

--- servertest/client.py
+++ servertest/client.py
@@ -1,7 +1,8 @@
 import socket
 # RASPBERRY PI RUNS THIS
+# 140.247.148.132 PI ADDR
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '140.247.0.16' #Need IP of the laptop
+host = '140.247.148.132' #Need IP of the laptop
 port = 9988
 s.connect((host, port))
 s.sendall('PLZ PRINT THIS, I WILL BE V HAPPY')
