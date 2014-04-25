c6421576e8501e0b709d166ca07d1b979a575581
Sean Walsh <swalsh50119@gmail.com>
2014-04-25 00:44:13 -0400

    updated_securiot

--- securiot.py
+++ securiot.py
@@ -5,10 +5,11 @@ import video
 import sys
 import smtplib
 import time
+from PIL import Image
 import io
 import picamera
-import glob
 import os
+import glob
 
 #  Goals of the algorithm
 #1 Detect the removal of a target object from the focus area
@@ -66,6 +67,14 @@ class App(object):
         files = glob.glob('./memory/*')
         for f in files:
             os.remove(f)
+
+    def take_pic(self,camera):
+        stream = io.BytesIO()
+        camera.capture(stream, format='jpeg', use_video_port=True)
+        stream.seek(0)
+        current_image = Image.open(stream)
+        img = cv2.cvtColor(np.asarray(current_image), cv2.COLOR_RGB2BGR)
+        return img
 
     def add_memory(self):
         files = glob.glob('./memory/*')
@@ -164,18 +173,15 @@ class App(object):
                 #Call Alert function
 
     def run(self):
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            stream = picamera.PiCameraCircularIO(camera, seconds=10)
+            camera.start_recording(stream, format='h264')
         while True:
-            #Read in the image, Draw Edges
-            #self.frame = cv2.imread('edgetest.png')
-            #ret, self.frame = self.cam.read()
-            stream = io.BytesIO()
-            with picamera.PiCamera() as camera:
-                camera.resolution = (640,480)
-                time.sleep(.3)
-                camera.capture(stream,format='jpeg')
-            data = np.fromstring(stream.getvalue(),dtype=np.uint8)
-            self.frame=cv2.imdecode(data,1)
-            #App.add_memory(self)
+            camera.wait_recording(1)
+            self.frame = App.take_pic(self,camera)
+            if not self.selection and not self.tracking_state:
+                #App.send_first()
             if self.selection and self.tracking_state:
                 for s in self.selection:
                     ind = self.selection.index(s)
@@ -202,10 +208,11 @@ class App(object):
                     App.check_obj(self,area_arr,ind)
                     winname = 'Focus' + str(ind)
                     #cv2.imshow(winname,preview)
-            ch = 0xFF & cv2.waitKey(30)
+            ch = 0xFF & cv2.waitKey(1)
             if ch == 27:
                 break
         cv2.destroyAllWindows()
+        camera.stop_recording()
 
 if __name__ == '__main__':
     #import socket
@@ -221,64 +228,3 @@ if __name__ == '__main__':
     except: video_src = 0
     print __doc__
     App(video_src).run()
-
-
-'''
-def detect_motion(camera):
-    global prior_image
-    stream = io.BytesIO()
-    camera.capture(stream, format='jpeg', use_video_port=True)
-    stream.seek(0)
-    if prior_image is None:
-        prior_image = Image.open(stream)
-        return False
-    else:
-        current_image = Image.open(stream)
-        # Compare current_image to prior_image to detect motion. This is
-        # left as an exercise for the reader!
-        result = random.randint(0, 10) == 0
-        # Once motion detection is done, make the prior image the current
-        prior_image = current_image
-        return result
-
-def write_video(stream):
-    # Write the entire content of the circular buffer to disk. No need to
-    # lock the stream here as we're definitely not writing to it
-    # simultaneously
-    with io.open('before.h264', 'wb') as output:
-        for frame in stream.frames:
-            if frame.header:
-                stream.seek(frame.position)
-                break
-        while True:
-            buf = stream.read1()
-            if not buf:
-                break
-            output.write(buf)
-    # Wipe the circular stream once we're done
-    stream.seek(0)
-    stream.truncate()
-
-with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
-    stream = picamera.PiCameraCircularIO(camera, seconds=10)
-    camera.start_recording(stream, format='h264')
-    try:
-        while True:
-            camera.wait_recording(1)
-            if detect_motion(camera):
-                print('Motion detected!')
-                # As soon as we detect motion, split the recording to
-                # record the frames "after" motion
-                camera.split_recording('after.h264')
-                # Write the 10 seconds "before" motion to disk as well
-                write_video(stream)
-                # Wait until motion is no longer detected, then split
-                # recording back to the in-memory circular buffer
-                while detect_motion(camera):
-                    camera.wait_recording(1)
-                print('Motion stopped!')
-                camera.split_recording(stream)
-    finally:
-        camera.stop_recording()
-'''
